02c952e46fc30b58a4d263db9a4ae9df6cc97623
Rakesh Kumar <rakesh.kumar@viennasolutions.com>
2021-09-28 15:25:22 +0530

    set conversion rate type

--- ViennaAdvantage/Areas/VA012/Models/StatementModel.cs
+++ ViennaAdvantage/Areas/VA012/Models/StatementModel.cs
@@ -67,15 +67,24 @@ namespace VA012.Models
         /// <param name="ctx">Context</param>
         /// <param name="fields">fields</param>
         /// <returns>PaymentMethodId</returns>
-        public string GetPaymentMethod(Ctx ctx, string fields)
+        public Dictionary<string, object> GetPaymentMethod(Ctx ctx, string fields)
         {
+            Dictionary<string, object> list = new Dictionary<string, object>();
             string[] paramValue = fields.Split(',');
             int id = Util.GetValueOfInt(paramValue[0].ToString());
+            DataSet ds = null;
             //1->Invoice & 2->Order PaymentMethodId
             if (Util.GetValueOfInt(paramValue[1].ToString()) == 1)
-                return Util.GetValueOfString(DB.ExecuteScalar("SELECT VA009_PAYMENTMETHOD_ID FROM C_INVOICE WHERE C_INVOICE_ID=" + id, null, null));
-            else
-                return Util.GetValueOfString(DB.ExecuteScalar("SELECT VA009_PAYMENTMETHOD_ID FROM C_ORDER WHERE C_ORDER_ID=" + id, null, null));
+                ds = DB.ExecuteDataset("SELECT VA009_PAYMENTMETHOD_ID,C_CONVERSIONTYPE_ID FROM C_INVOICE WHERE C_INVOICE_ID=" + id, null, null);
+            else if (Util.GetValueOfInt(paramValue[1].ToString()) == 2)
+                ds = DB.ExecuteDataset("SELECT VA009_PAYMENTMETHOD_ID,C_CONVERSIONTYPE_ID FROM C_ORDER WHERE C_ORDER_ID=" + id, null, null);
+            
+            if (ds != null && ds.Tables[0].Rows.Count > 0)
+            {
+                list["VA009_PaymentMethod_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["VA009_PAYMENTMETHOD_ID"]);
+                list["C_ConversionType_ID"] = Util.GetValueOfInt(ds.Tables[0].Rows[0]["C_CONVERSIONTYPE_ID"]);
+            }
+            return list;
         }
     }
 }
