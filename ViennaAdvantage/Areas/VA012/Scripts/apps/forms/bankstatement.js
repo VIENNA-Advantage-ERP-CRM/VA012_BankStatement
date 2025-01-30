c825540f863f44ad776f6fd11068081a0f112daa
ViennaRU <builduser@viennasolutions.com>
2025-01-30 14:44:29 +0000

    pushed to origin

--- ViennaAdvantage/Areas/VA012/Scripts/apps/forms/bankstatement.js
+++ ViennaAdvantage/Areas/VA012/Scripts/apps/forms/bankstatement.js
@@ -5521,7 +5521,9 @@
                         _btnNewRecord.attr("activestatus", "0");
                         //Remove the back ground color of high lighted grid
                         BankStatementLineIdForSave = 0;
-                        EdiStatementtRecordDiv.removeClass('VA012-EditRecordBackColor');
+                        if (EdiStatementtRecordDiv != null && EdiStatementtRecordDiv != undefined) {
+                            EdiStatementtRecordDiv.removeClass('VA012-EditRecordBackColor');
+                        }
                         //_btnNewRecord.attr("src", "Areas/VA012/Images/add.png");
                         _btnNewRecord.addClass("vis vis-plus");
                         _btnNewRecord.removeClass("fa fa-minus");
@@ -5552,7 +5554,9 @@
                     //}
                     //else {
                     BankStatementLineIdForSave = 0;
-                    EdiStatementtRecordDiv.removeClass('VA012-EditRecordBackColor');
+                    if (EdiStatementtRecordDiv != null && EdiStatementtRecordDiv != undefined) {
+                        EdiStatementtRecordDiv.removeClass('VA012-EditRecordBackColor');
+                    }
                     newRecordForm.scheduleRefresh();
                     newRecordForm.prepayRefresh();
                     newRecordForm.refreshForm();
